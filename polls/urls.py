bdc4351f7f134e4c3162f4f49fdd7621a863c10b
avierkin03 <avierkin03@gmail.com>
2025-09-25 22:06:06 +0300

    added models and 2 views for poll app

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    
+    # Список активних опитувань
+    path('', views.PollListView.as_view(), name='poll_list'),
+    # Деталі опитування
+    path('<int:pk>/', views.PollDetailView.as_view(), name='poll_detail'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, View
+from .models import Poll, PollOption, PollVote
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+# Список активних опитувань
+class PollListView(ListView):
+    model = Poll
+    template_name = 'polls/poll_list.html'
+    context_object_name = 'polls'
+
+    def get_queryset(self):
+        return Poll.objects.filter(is_active=True)
+
+
+# Деталі опитування, включаючи перевірку, чи голосував користувач
+class PollDetailView(DetailView):
+    model = Poll
+    template_name = 'polls/poll_detail.html'
+    context_object_name = 'poll'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Додаємо інформацію, чи голосував користувач
+        if self.request.user.is_authenticated:
+            context['has_voted'] = PollVote.objects.filter(poll=self.object, user=self.request.user).exists()
+        return context
