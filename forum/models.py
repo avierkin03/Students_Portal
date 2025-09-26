754fdeabc05c4bc79e8dcc7594f8c82622a1efb0
DanyaStorozhuk <stjdanya24@gmail.com>
2025-09-26 17:46:42 +0300

    update my file

--- forum/models.py
+++ forum/models.py
@@ -1,28 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
-class Form(models.Model):
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-    
-class FormField(models.Model):
-    name = models.CharField(max_length=50)
-    phone = models.CharField(max_length=20, blank=True, null=True)
-    email = models.EmailField()
-    message = models.TextField()
-    date_create = models.DateTimeField(auto_now_add=True)
+# тема для обговорення 
+class Topic(models.Model):
+    name = models.CharField(max_length=200, verbose_name = "topic name")
+    title = models.TextField(verbose_name="title")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner_topic")
+    date_create = models.DateTimeField(auto_now_add=True, verbose_name="create_date")
 
     def __str__(self):
         return self.name
+    
 
-class FormSubmite(models.Model):
-    form = models.ForeignKey(Form, on_delete=models.CASCADE, related_name="submitions")
-    submite_date = models.DateTimeField(auto_now_add=True)
-    data = models.JSONField()
+# пост/повідомлення окнкретного юзера
+class Post(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name="posts")
+    text = models.TextField(verbose_name="title-post")
+    owner_post = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner_post")
+    date_create = models.DateTimeField(auto_now_add=True, verbose_name="create_date")
 
     def __str__(self):
-        return self.form.title
+        return f"Post in topic: {self.topic.name} from {self.owner_post.username}"
+
--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
+from forum import models 
 # Create your views here.
+
+class ClassForumList(ListView):
+    model = models.Topic
+    context_object_name = "forum_list"
+    template_name = "forum_list.html"
+
+
+class ClassForumDetail(DetailView):
+    model = models.Post
+    context_object_name = "forum_detail"
+    template_name = "forum_detail.html"
+
+
+
+
+
