c9a6f3c807e0246f11a2f6d386aaa5943dd289ee
NHelixo <167092094+NHelixo@users.noreply.github.com>
2025-10-03 17:18:34 +0300

    +

--- announcements/urls.py
+++ announcements/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AnnouncementListView
-
+from .views import AnnouncementListView, AnnouncementInfoView, AnnouncementCreateView
 app_name = 'announcements'
 
 urlpatterns = [
     path('', AnnouncementListView.as_view(), name='announcement_list'),
+    path("<int:pk>/", AnnouncementInfoView.as_view(), name="announcement_detail"),
+    path("create/", AnnouncementCreateView.as_view(), name="announcement_create"),
 ]
--- announcements/views.py
+++ announcements/views.py
@@ -1,8 +1,27 @@
+from django.views.generic import CreateView
 from django.views import View
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.shortcuts import render, get_object_or_404
 from .models import Announcement
 
 class AnnouncementListView(View):
     def get(self, request):
         announcement = Announcement.objects.all()
         return render(request, "announcements/announcement_list.html", {'announcement_list': announcement})
+    
+
+class AnnouncementInfoView(View):
+    def get(self, request, pk):
+        announcement = get_object_or_404(Announcement, pk=pk)
+        return render(request, "announcements/announcements_info.html", {"announcement": announcement})
+
+
+class AnnouncementCreateView(CreateView):
+    model = Announcement
+    template_name = "announcements/announcement_create.html"
+    fields = ["title", "text"]
+    success_url = reverse_lazy("announcements:announcement_list")
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
