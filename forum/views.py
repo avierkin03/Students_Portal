7f4197ffa0addf74e8c33880c1315432710017b7
DanyaStorozhuk <stjdanya24@gmail.com>
2025-10-13 15:47:56 +0200

    Udpdate

--- forum/views.py
+++ forum/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from forum import models 
 from forum.form import TopicForm, PostForm
 from forum.mixings import UserPassesTestMixin, LoginRequiredMixin
+from django.urls import reverse_lazy
 # Create your views here.
 
 class ClassForumList(ListView):
@@ -23,22 +24,28 @@ class TopicCreateView(LoginRequiredMixin, CreateView):
     model = models.Topic
     form_class = TopicForm
     template_name = "forum/forum_create.html"
+    success_url = reverse_lazy("forum:forum_list")
 
 
 class TopicDeleteView(LoginRequiredMixin, DeleteView):
     model = models.Topic
+    context_object_name = "topic_delete"
     template_name = "forum/forum_delete.html"
+    success_url = reverse_lazy("forum:forum_list")
 
 
 class TopicUpdateView(LoginRequiredMixin, UpdateView):
     model = models.Topic
+    context_object_name = "topic"
     template_name = "forum/forum_update.html"
+    success_url = reverse_lazy("forum:forum_list")
 
 
 class PostCreateView(CreateView):
     model = models.Post
     form_class = PostForm
     template_name = "forum/post_create.html"
+    success_url = reverse_lazy('forum:forum_list')
 
 class PostDeleteView(LoginRequiredMixin, DeleteView ):
     model = models.Post
