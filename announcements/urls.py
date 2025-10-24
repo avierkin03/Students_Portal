4267e7aeddb6585f259eac49bedbc1b5a439b382
avierkin03 <avierkin03@gmail.com>
2025-10-24 17:59:00 +0300

    added emojies in announcements app

--- announcements/urls.py
+++ announcements/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AnnouncementListView, AnnouncementInfoView, AnnouncementCreateView, AnnouncementDeleteView
+from .views import AnnouncementListView, AnnouncementInfoView, AnnouncementCreateView, AnnouncementDeleteView, add_reaction
 app_name = 'announcements'
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path("<int:pk>/", AnnouncementInfoView.as_view(), name="announcement_detail"),
     path("create/", AnnouncementCreateView.as_view(), name="announcement_create"),
     path("<int:pk>/delete/", AnnouncementDeleteView.as_view(), name="announcement_delete"),
+    path("<int:pk>/react/<str:emoji>/", add_reaction, name="add_reaction"),
 ]
